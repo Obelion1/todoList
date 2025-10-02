d8dabe9803625bc360553d0a612d3e7c82bd17a4
Marco Scheidhuber <github@mail.j23d.de>
2025-10-02 06:52:01 +0000

    Added functionality to add a todo via PHP.

--- todo-api.php
+++ todo-api.php
@@ -1,5 +1,22 @@
 <?php
+header('Content-Type: application/json');
 
-echo "PHP is working";
+// Read current todos in json file
+$file = 'todo.json';
+if (file_exists($file)) {
+    $json_data = file_get_contents($file);
+    $todos = json_decode($json_data, true);
+} else {
+    $todos = [];
+}
 
-?>
+// Add new entry to json file
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = file_get_contents('php://input');
+    $input = json_decode($data, true);
+    $todos[] = $input['todo'];
+    file_put_contents($file, json_encode($todos));
+    echo json_encode(['status' => 'success']);
+    exit;
+}
+
